8862f1575211a54757a23fe043014795ced4df0f
Himanshu <him@email.com>
2025-06-17 13:10:20 +0530

    [Himanshu Singh].Add Arrays day 4

---
+++ Arrays/q1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class q1{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the value n for table");
+int n= sc.nextInt();
+for (int i=1;i<=10;i++){
+System.out.println(n+"*"+i+"="+n*i);
+}
+}
+}
